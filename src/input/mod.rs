b011154113ea11c5df98a874f91c013d6d195b76
Johan Ablett-Karlsson <johan.ellfries@gmail.com>
2020-06-11 09:01:13 +0200

    feat(parse yaml): create first working test on parsing yaml #wip

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,15 +1,60 @@
+extern crate yaml_rust;
+
+use yaml_rust::{YamlLoader, Yaml};
+
 struct Sutra {
-    
-    sutras: Vec![Sutra]
+    name: String,
+    desc: Option<String>,
+    n: Vec<Sutra>
 }
 
+impl Sutra {
+    fn new<T> (name: T, desc: Option<T>, n: Vec<Sutra>) -> Sutra 
+    where T: Into<String>
+    {
+        Sutra { name: name.into(), desc: desc.map(|s| s.into()), n }
+    }
+}
 
 pub fn load_sutra_from_yaml() {
     println!("loading sutra")
 }
 
-fn parse_yaml_string(yaml: str) {
+fn parse_yaml_string(yaml_string: &str) -> Result<Vec<Sutra>, &'static str> {
+    let yaml = YamlLoader::load_from_str(yaml_string).unwrap();
+
+    Ok(vec!(parse_node(&yaml[0]["n"][0])))
 }
+
+fn read_str<'a>(yaml: Option<Yaml>) -> Option<String> {
+    yaml.map(|y| match y {
+        Yaml::String(str) => Some(str),
+        _ => None
+    }).flatten()
+}
+
+fn parse_node<'a> (hash: &'a Yaml) -> Sutra {
+    print!("{:?}", hash);
+    let name = hash["name"].as_str().unwrap();
+
+    let desc = hash["desc"].as_str();
+    let nodes = match hash["n"].as_vec() {
+        Some(vec) => 
+            vec
+            .iter()
+            .map(|n| parse_node(n))
+            .collect(),
+        None => 
+            Vec::new()
+    };
+
+   Sutra::new(name, desc, nodes)
+}
+
+// Parse main element 
+// find n:
+// For each element add name: desc: (optional), add children optional
+
 
 #[cfg(test)]
 mod tests {
@@ -17,6 +62,20 @@ mod tests {
 
     #[test]
     fn it_parses_yaml_when_well_formatted() {
+        let yaml = "n:
+    - name: How to Use Mind Sutras
+      desc: 'This is about how you can use the mind sutras project
+      to create cool mind maps.'
+      n:
+        - name: Format
+          desc: You write your document '.msutras' in yaml format.
+        - name: Save
+          desc: Saving will update the rendering of your document.
+";
+        let sut = parse_yaml_string(&yaml).unwrap();
+
+        assert_eq!(sut.len(), 1);
+        assert_eq!(sut[0].n.len(), 2);
     }
 
 }
